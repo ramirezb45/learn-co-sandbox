023ea48b06ba0c64076a530e41f47a8e6cd537bc
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-26 22:19:10 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,25 @@
+def greeting
+  puts "Hello World"
+end
+
+
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+
+
+say_greeting_five_times
+
+def greeting(name)
+  puts "Hello, #{name}!"
+end
+
+greeting("Sophie")
+
+
